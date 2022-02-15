3faa7b6f8787d626b8db8b506363c2d9239f04d1
FutureSixx <64593024+FutureSixx@users.noreply.github.com>
2022-02-15 15:26:48 -0600

    Update VestScripts.c

--- Scripts/4_World/VestScripts.c
+++ Scripts/4_World/VestScripts.c
@@ -1,5 +1,5 @@
 class ModularVestSystem_Base extends Clothing
-{
+{ 
 
 	override bool CanPutInCargo( EntityAI parent )
 	{
@@ -41,7 +41,15 @@ class ModularVestSystem_Base extends Clothing
         }		
         return super.CanReceiveAttachment(attachment, slotId);
     }
-}
+};
+class MVS_Combat_Vest_OD: ModularVestSystem_Base {};
+class MVS_Combat_Vest_ERDL: ModularVestSystem_Base {};
+class MVS_Combat_Vest_Tan: ModularVestSystem_Base {};
+class MVS_Combat_Vest_Black: ModularVestSystem_Base {};
+class MVS_Combat_Vest_Snow: ModularVestSystem_Base {};
+class MVS_Combat_Vest_Multicam: ModularVestSystem_Base {};
+class MVS_Combat_Vest_Multicam_Black: ModularVestSystem_Base {};
+class MVS_Combat_Vest_Multicam_Tropic: ModularVestSystem_Base {};
 	
 class Modular_Belt_Base extends Clothing
 {
@@ -86,7 +94,16 @@ class Modular_Belt_Base extends Clothing
         }		
         return super.CanReceiveAttachment(attachment, slotId);
     }
-}
+};
+
+class MVS_Belt_OD: Modular_Belt_Base {};
+class MVS_Belt_ERDL: Modular_Belt_Base {};
+class MVS_Belt_Tan: Modular_Belt_Base {};
+class MVS_Belt_Black: Modular_Belt_Base {};
+class MVS_Belt_Snow: Modular_Belt_Base {};
+class MVS_Belt_Multicam: Modular_Belt_Base {};
+class MVS_Belt_Multicam_Black: Modular_Belt_Base {};
+class MVS_Belt_Multicam_Tropic: Modular_Belt_Base {};
 
 class ModularVestSystem_Heavy extends Clothing
 {
@@ -105,7 +122,16 @@ class ModularVestSystem_Heavy extends Clothing
 		
 		return IsEmpty();
 	}
-}
+};
+
+class MVS_Combat_Vest_Heavy_OD: ModularVestSystem_Heavy {};
+class MVS_Combat_Vest_Heavy_ERDL: ModularVestSystem_Heavy {};
+class MVS_Combat_Vest_Heavy_Tan: ModularVestSystem_Heavy {};
+class MVS_Combat_Vest_Heavy_Black: ModularVestSystem_Heavy {};
+class MVS_Combat_Vest_Heavy_Snow: ModularVestSystem_Heavy {};
+class MVS_Combat_Vest_Heavy_Multicam: ModularVestSystem_Heavy {};
+class MVS_Combat_Vest_Heavy_Multicam_Tropic: ModularVestSystem_Heavy {};
+class MVS_Combat_Vest_Heavy_Multicam_Black: ModularVestSystem_Heavy {};
 
 class ModularChestRig_Base extends Clothing
 {
@@ -150,4 +176,13 @@ class ModularChestRig_Base extends Clothing
         }		
         return super.CanReceiveAttachment(attachment, slotId);
     }
-}
+};
+
+class MVS_ChestRig_OD: ModularChestRig_Base {};
+class MVS_ChestRig_ERDL: ModularChestRig_Base {};
+class MVS_ChestRig_Tan: ModularChestRig_Base {};
+class MVS_ChestRig_Black: ModularChestRig_Base {};
+class MVS_ChestRig_Snow: ModularChestRig_Base {};
+class MVS_ChestRig_Multicam: ModularChestRig_Base {};
+class MVS_ChestRig_Multicam_Tropic: ModularChestRig_Base {};
+class MVS_ChestRig_Multicam_Black: ModularChestRig_Base {};
